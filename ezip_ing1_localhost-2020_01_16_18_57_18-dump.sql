70bdf6a982a2d9aebf7bc3ed67617bd2c9b013a4
dieng25 <moussa.dieng@etu.u-pec.fr>
2025-02-07 17:25:52 +0100

    fichier sql modifé

--- ezip_ing1_localhost-2020_01_16_18_57_18-dump.sql
+++ ezip_ing1_localhost-2020_01_16_18_57_18-dump.sql
@@ -1,13 +1,35 @@
 --
--- Name: students; Type: TABLE; Schema: ezip_ing1
---
 
-CREATE TABLE students (
-    name varchar(64) NOT NULL,
-    firstname varchar(64) NOT NULL,
-    groupname varchar(8) NOT NULL,
-    id int(20) NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id)
+--
+CREATE TABLE Citoyen(
+   tel_num VARCHAR(50),
+   Nom VARCHAR(50) NOT NULL,
+   Prénom VARCHAR(50) NOT NULL,
+   email VARCHAR(50),
+   PRIMARY KEY(tel_num),
+   UNIQUE(email)
 );
 
-INSERT INTO students (name, firstname, groupname) VALUES ("ANONYMOUS", "Stephane", "Admin");
+CREATE TABLE Mairie(
+   Code_Postal VARCHAR(50),
+   email VARCHAR(50),
+   mdp VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Code_Postal),
+   UNIQUE(email)
+);
+
+CREATE TABLE Incident_Suggestion(
+   Id_ticket VARCHAR(50),
+   Type VARCHAR(50) NOT NULL,
+   Description VARCHAR(50) NOT NULL,
+   date_emis DATE NOT NULL,
+   Catégorie VARCHAR(50) NOT NULL,
+   Statut VARCHAR(50) NOT NULL,
+   CP_Incident VARCHAR(50) NOT NULL,
+   Priorité VARCHAR(50),
+   tel_num VARCHAR(50) NOT NULL,
+   Code_Postal VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_ticket),
+   FOREIGN KEY(tel_num) REFERENCES Citoyen(tel_num),
+   FOREIGN KEY(Code_Postal) REFERENCES Mairie(Code_Postal)
+);
