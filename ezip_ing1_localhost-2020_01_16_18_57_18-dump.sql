7ae59fac84e20a0a74350db5a7f204f19fbb1e5e
GlabGlab <claraadamaly@gmail.com>
2025-02-17 12:48:58 +0100

    modification nom champ bdd

--- ezip_ing1_localhost-2020_01_16_18_57_18-dump.sql
+++ ezip_ing1_localhost-2020_01_16_18_57_18-dump.sql
@@ -2,36 +2,36 @@
 
 --
 CREATE TABLE Citoyen(
-tel_num VARCHAR(50),
-Nom VARCHAR(50) NOT NULL,
-Prénom VARCHAR(50) NOT NULL,
-email VARCHAR(50),
-PRIMARY KEY(tel_num),
-UNIQUE(email)
+        tel_num VARCHAR(50),
+        Nom VARCHAR(50) NOT NULL,
+        Prénom VARCHAR(50) NOT NULL,
+        email VARCHAR(50),
+        PRIMARY KEY(tel_num),
+        UNIQUE(email)
 );
 
 CREATE TABLE Mairie(
-Code_Postal VARCHAR(50),
-email VARCHAR(50),
-mdp VARCHAR(50) NOT NULL,
-PRIMARY KEY(Code_Postal),
-UNIQUE(email)
+       Code_Postal VARCHAR(50),
+       email VARCHAR(50),
+       mdp VARCHAR(50) NOT NULL,
+       PRIMARY KEY(Code_Postal),
+       UNIQUE(email)
 );
 
 CREATE TABLE Incident_Suggestion(
-Id_ticket VARCHAR(50),
-Type VARCHAR(50) NOT NULL,
-Titre VARCHAR(50) NOT NULL,
-Description VARCHAR(50) NOT NULL,
-date_emis DATE NOT NULL,
-Catégorie VARCHAR(50) NOT NULL,
-Statut VARCHAR(50) NOT NULL,
-CP_Incident VARCHAR(50) NOT NULL,
-Priorité VARCHAR(50),
-tel_num VARCHAR(50) NOT NULL,
-Code_Postal VARCHAR(50) NOT NULL,
-PRIMARY KEY(Id_ticket),
-FOREIGN KEY(tel_num) REFERENCES Citoyen(tel_num),
-FOREIGN KEY(Code_Postal) REFERENCES Mairie(Code_Postal)
+            Id_ticket VARCHAR(50),
+            Type VARCHAR(50) NOT NULL,
+            Titre VARCHAR(50) NOT NULL,
+            Description VARCHAR(50) NOT NULL,
+            date_emis DATE NOT NULL,
+            Catégorie VARCHAR(50) NOT NULL,
+            Statut VARCHAR(50) NOT NULL,
+            CP_Ticket VARCHAR(50) NOT NULL,
+            Priorité VARCHAR(50),
+            tel_num VARCHAR(50) NOT NULL,
+            Code_Postal VARCHAR(50) NOT NULL,
+            PRIMARY KEY(Id_ticket),
+            FOREIGN KEY(tel_num) REFERENCES Citoyen(tel_num),
+            FOREIGN KEY(Code_Postal) REFERENCES Mairie(Code_Postal)
 );
 
