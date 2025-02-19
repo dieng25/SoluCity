4dc4bf3a2c8e98f2b98bef1fef4f19f810938bfd
GlabGlab <claraadamaly@gmail.com>
2025-02-19 16:38:03 +0100

    dto Citoyen.java

--- xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Citoyen.java
+++ xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Citoyen.java
@@ -3,6 +3,11 @@ package edu.ezip.ing1.pds.business.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
+import java.lang.reflect.Field;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @JsonRootName(value = "citoyen")
 public class Citoyen {
     private String telNum;
@@ -70,13 +75,42 @@ public class Citoyen {
         this.identifiant = identifiant;
     }
 
+    public final Citoyen build(final ResultSet resultSet)
+            throws SQLException, NoSuchFieldException, IllegalAccessException {
+        setFieldsFromResulset(resultSet, "telNum", "nom","prenom","email","identifiant");
+        return this;
+    }
+    public final PreparedStatement build(PreparedStatement preparedStatement)
+            throws SQLException, NoSuchFieldException, IllegalAccessException {
+        return buildPreparedStatement(preparedStatement, telNum, nom, prenom, email, identifiant);
+    }
+
+    private void setFieldsFromResulset(final ResultSet resultSet, final String ... fieldNames )
+            throws NoSuchFieldException, SQLException, IllegalAccessException {
+        for(final String fieldName : fieldNames ) {
+            final Field field = this.getClass().getDeclaredField(fieldName);
+            field.set(this, resultSet.getObject(fieldName));
+        }
+    }
+
+    private final PreparedStatement buildPreparedStatement(PreparedStatement preparedStatement, final String ... fieldNames )
+            throws NoSuchFieldException, SQLException, IllegalAccessException {
+        int ix = 0;
+        for(final String fieldName : fieldNames ) {
+            preparedStatement.setString(++ix, fieldName);
+        }
+        return preparedStatement;
+    }
+
     @Override
     public String toString() {
-        return "Citoyen [telNum=" + telNum + ", " +
-                "nom=" + nom +
-                ", prenom=" + prenom +
-                ", email=" + email +
-                ", identifiant=" + identifiant + "]";
+        return "Citoyen{" +
+                "telNum='" + telNum + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", identifiant='" + identifiant + '\'' +
+                '}';
     }
 }
 
