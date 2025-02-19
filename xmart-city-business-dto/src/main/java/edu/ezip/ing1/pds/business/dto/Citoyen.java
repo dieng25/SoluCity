cdedeab70ff6176b09cec1f182a185a5e6f5cd40
GlabGlab <claraadamaly@gmail.com>
2025-02-19 10:07:29 +0100

    dto Citoyen

--- xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Citoyen.java
+++ xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Citoyen.java
@@ -1,5 +1,6 @@
 package edu.ezip.ing1.pds.business.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName(value = "citoyen")
@@ -9,9 +10,73 @@ public class Citoyen {
     private String prenom;
     private String email;
     private String identifiant;
+
+    public Citoyen () {
+    }
+
+    public Citoyen(String telNum, String nom, String prenom, String email, String identifiant){
+        this.telNum = telNum;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.identifiant = identifiant;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getIdentifiant() {
+        return identifiant;
+    }
+
+    @JsonProperty("citoyen_telNum")
+    public void setTelNum(String telNum) {
+        if (telNum == null || !telNum.matches("^0\\d{9}$")) {
+            throw new IllegalArgumentException("Le numéro de téléphone doit commencer par 0 et contenir exactement 10 chiffres.");
+        }
+        this.telNum = telNum;
+    }
+
+    @JsonProperty("citoyen_nom")
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @JsonProperty("citoyen_prenom")
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    @JsonProperty("citoyen_email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("citoyen_identifiant")
+    public void setIdentifiant(String identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    @Override
+    public String toString() {
+        return "Citoyen [telNum=" + telNum + ", " +
+                "nom=" + nom +
+                ", prenom=" + prenom +
+                ", email=" + email +
+                ", identifiant=" + identifiant + "]";
+    }
 }
 
-public Citoyen () {
-}
-
-public Citoyen(String telN)
