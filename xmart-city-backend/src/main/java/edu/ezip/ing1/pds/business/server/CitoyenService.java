8f2a7282a521887d9c97d1c4bdf22a4bf2a53e98
GlabGlab <claraadamaly@gmail.com>
2025-02-20 13:48:31 +0100

    Modification Incident.java ajout settter et getter, CitoyenService.java, SoluCityService.java

--- xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/CitoyenService.java
+++ xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/CitoyenService.java
@@ -1,26 +1,44 @@
 package edu.ezip.ing1.pds.business.server;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ezip.ing1.pds.business.dto.Citoyen;
+import edu.ezip.ing1.pds.business.dto.Incident;
 import edu.ezip.ing1.pds.commons.Request;
 import edu.ezip.ing1.pds.commons.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Random;
 
 public class CitoyenService {
 
+    private final Logger logger = LoggerFactory.getLogger("Business-Server");
+
     private enum Queries {
         INSERT_CITOYEN("INSERT INTO Citoyen (tel_num, Nom, Prénom, email, Identifiant) VALUES (?, ?, ?, ?, ?)"),
-        INSERT_INCIDENT("INSERT INTO Incident (Titre, Description, date_emis, Catégorie, Statut, CodePostal_ticket, Priorité, tel_num) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"),
-        INSERT_SUGGESTION("INSERT INTO Suggestion (Titre, Description, date_emis, Catégorie, statut, CodePostal_ticket, tel_num) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        INSERT_INCIDENT("INSERT INTO Incident (Titre, Description, date_emis, Catégorie, Statut, CodePostal_ticket, Priorité) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        //SELECT_INCIDENT("SELECT * FROM Incident WHERE Id_ticket = ? AND tel_num = ?"),
+        //UPDATE_INCIDENT("UPDATE Incident SET Titre = ?, Description = ? WHERE Id_ticket = ? AND Statut = 0");
         private final String query;
 
         private Queries(final String query) {
             this.query = query;
         }
 
+    }
+
+    public static CitoyenService inst = null;
+
+    public static final CitoyenService getInstance() {
+        if (inst == null) {
+            inst = new CitoyenService();
+        }
+        return inst;
     }
 
     private CitoyenService() {
@@ -30,7 +48,7 @@ public class CitoyenService {
             throws InvocationTargetException, IllegalAccessException, SQLException, IOException {
         Response response = null;
 
-        final CitoyenService.Queries queryEnum = Enum.valueOf(CitoyenService.Queries.class, request.getRequestOrder());
+        final Queries queryEnum = Enum.valueOf(CitoyenService.Queries.class, request.getRequestOrder());
         switch(queryEnum) {
             case INSERT_CITOYEN:
                 response = InsertCitoyen(request, connection);
@@ -38,16 +56,83 @@ public class CitoyenService {
             case INSERT_INCIDENT:
                 response = InsertIncident(request, connection);
                 break;
-            case INSERT_SUGGESTION:
-                response = InsertSuggestion(request, connection);
-                break;
-            default:
-                break;
+            //case SELECT_INCIDENT:
+               // response = selectIncident(request, connection);
+               // break;
+            //case UPDATE_INCIDENT:
+              //  response = updateIncident(request, connection);
+              //  break;
+           // default:
+             //   break;
+       }
+
+       return response;
+    }
+
+    private Response InsertCitoyen(final Request request, final Connection connection) throws SQLException, IOException {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        Citoyen citoyen = objectMapper.readValue(request.getRequestBody(), Citoyen.class);
+
+        //Crée un Identifiant Unique -> Champ Identifiant table Citoyen
+        String identifiant = generateIdentifiant(citoyen.getPrenom(), citoyen.getNom());
+        citoyen.setIdentifiant(identifiant);
+
+        PreparedStatement stmt = connection.prepareStatement(CitoyenService.Queries.INSERT_CITOYEN.query);
+        stmt.setString(1, citoyen.getTelNum());
+        stmt.setString(2, citoyen.getNom());
+        stmt.setString(3, citoyen.getPrenom());
+        stmt.setString(4, citoyen.getEmail());
+        stmt.setString(5, citoyen.getIdentifiant());
+        int rowsInserted = stmt.executeUpdate();
+        return new Response(request.getRequestId(), objectMapper.writeValueAsString(citoyen));
+    }
+
+    private String generateIdentifiant(String prenom, String nom) {
+        String firstLetterPrenom = prenom.substring(0, 1).toUpperCase();
+        String firstLetterNom = nom.substring(0, 1).toUpperCase();
+        String lastLetterNom = nom.substring(nom.length() - 1).toUpperCase();
+        String randomPart = generateRandomString(4);
+        return firstLetterPrenom + firstLetterNom + lastLetterNom + randomPart;
+    }
+
+    private String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return sb.toString();
+    }
+
+
+    private Response InsertIncident(final Request request, final Connection connection) throws SQLException, IOException {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        PreparedStatement stmt = connection.prepareStatement(Queries.INSERT_INCIDENT.query, Statement.RETURN_GENERATED_KEYS);
+        Incident incident = objectMapper.readValue(request.getRequestBody(), Incident.class);
+        stmt.setString(1, incident.getTitre());
+        stmt.setString(2, incident.getDescription());
+        stmt.setString(3, incident.getDate());
+        stmt.setString(4, incident.getCategorie());
+        stmt.setInt(5, incident.getStatut());
+        stmt.setInt(6, incident.getCP_Ticket());
+        stmt.setInt(7, incident.getPriorite());
+        int rowsInserted = stmt.executeUpdate();
+
+
+        ResultSet generatedKeys = stmt.getGeneratedKeys();
+        if (generatedKeys.next()) {
+            incident.setIdTicket(generatedKeys.getInt(1));
         }
 
-        return response;
+        return new Response(request.getRequestId(), objectMapper.writeValueAsString(incident));
     }
 
 
 
+
 }
+
+
+
+
--- xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/SoluCityService.java
+++ xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/SoluCityService.java
@@ -23,7 +23,7 @@ public class SoluCityService {
     }
 
     private SoluCityService() {
-        CitoyenService citoyenService = new CitoyenService();
+
     }
 
 
