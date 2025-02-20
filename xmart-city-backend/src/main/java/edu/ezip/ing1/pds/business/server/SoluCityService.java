5f5aacaaaac7c84a9bc1ace0ffbc2af6fbfb904f
GlabGlab <claraadamaly@gmail.com>
2025-02-20 11:17:08 +0100

    SoluCityService.java et CitoyenService.java

---
+++ xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/SoluCityService.java
@@ -0,0 +1,30 @@
+package edu.ezip.ing1.pds.business.server;
+
+import edu.ezip.ing1.pds.commons.Request;
+import edu.ezip.ing1.pds.commons.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SoluCityService {
+    private final static String LoggingLabel = "B u s i n e s s - S e r v e r";
+    private final Logger logger = LoggerFactory.getLogger(LoggingLabel);
+
+    public static SoluCityService inst = null;
+    public static final SoluCityService getInstance()  {
+        if(inst == null) {
+            inst = new SoluCityService();
+        }
+        return inst;
+    }
+
+    private SoluCityService() {
+        CitoyenService citoyenService = new CitoyenService();
+    }
+
+
+}
