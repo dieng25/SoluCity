a8bdd570ade7a43bc0e75b39c5b809d597d3e07f
GlabGlab <claraadamaly@gmail.com>
2025-03-05 11:49:53 +0100

    ajout request SelectAllMairiesClientRequest.java

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/requests/SelectAllMairiesClientRequest.java
@@ -0,0 +1,25 @@
+package edu.ezip.ing1.pds.requests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ezip.ing1.pds.business.dto.Mairies;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+
+import java.io.IOException;
+
+public class SelectAllMairiesClientRequest extends ClientRequest<Object, Mairies> {
+
+    public SelectAllMairiesClientRequest(
+            NetworkConfig networkConfig, int myBirthDate, Request request, Object info, byte[] bytes)
+            throws IOException {
+        super(networkConfig, myBirthDate, request, info, bytes);
+    }
+
+    @Override
+    public Mairies readResult(String body) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final Mairies mairies = mapper.readValue(body, Mairies.class);
+        return mairies;
+    }
+}
