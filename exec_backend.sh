61ba4c25898f597ab62bc12c4d13b4fdd720703d
GlabGlab <claraadamaly@gmail.com>
2025-03-09 07:57:02 +0100

    ajout script pour execution backend depuis la vm

---
+++ exec_backend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Variable
+user_name="solucityback"
+host_name="172.31.249.224" #ip vm solucity backend
+file="backend.jar"
+
+ssh $user_name@$host_name <<EOF
+    echo "Connexion réussie à la VM : $host_name"
+	
+    if [[ ! -f "$file" ]]; then
+        echo "Erreur : fichier $file introuvable sur la VM"
+        exit 1
+    fi
+	java -jar $file
+EOF
