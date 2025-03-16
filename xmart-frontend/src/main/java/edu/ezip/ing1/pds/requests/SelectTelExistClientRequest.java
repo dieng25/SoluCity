f61bb5eece31579eb48227d4873fa76763df6e4d
GlabGlab <claraadamaly@gmail.com>
2025-03-16 08:06:22 +0100

    création de SelectTelExistClientRequest.java

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/requests/SelectTelExistClientRequest.java
@@ -0,0 +1,23 @@
+package edu.ezip.ing1.pds.requests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+
+import java.io.IOException;
+
+public class SelectTelExistClientRequest extends ClientRequest<Object, Boolean> {
+
+    public SelectTelExistClientRequest(
+            NetworkConfig networkConfig, int myBirthDate, Request request, Object info, byte[] bytes)
+            throws IOException {
+        super(networkConfig, myBirthDate, request, info, bytes);
+    }
+
+    @Override
+    public Boolean readResult(String body) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(body, Boolean.class);
+    }
+}
