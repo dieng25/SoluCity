00aa5853cef525c82a8efab5bdc208ee9ae6b8f8
dieng25 <moussa.dieng@etu.u-pec.fr>
2025-03-16 22:04:46 +0100

    modification service incidents

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/requests/SelectAllIncidentsClientRequest.java
@@ -0,0 +1,26 @@
+package edu.ezip.ing1.pds.requests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.ezip.ing1.pds.business.dto.Incident;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+import java.io.IOException;
+import edu.ezip.ing1.pds.business.dto.Incidents;
+
+public class SelectAllIncidentsClientRequest extends ClientRequest<Object, Incidents> {
+
+    public SelectAllIncidentsClientRequest(
+            NetworkConfig networkConfig, int myBirthDate, Request request, Incident info, byte[] bytes)
+            throws IOException {
+        super(networkConfig, myBirthDate, request, info, bytes);
+    }
+
+    @Override
+    public Incidents readResult(String body) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final Incidents incidents = mapper.readValue(body, Incidents.class);
+        return incidents;
+    }
+}
