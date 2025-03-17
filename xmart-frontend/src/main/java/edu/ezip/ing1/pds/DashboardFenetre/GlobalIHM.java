91cad9de311518161ef059995f1082b6b08c9b4b
Alioune930 <tiendrebeogoali32@gmail.com>
2025-03-17 13:33:59 +0100

    modification du requesthandler
    ajout des attributs dateDebut, dateFin et codePostal

--- xmart-frontend/src/main/java/edu/ezip/ing1/pds/DashboardFenetre/GlobalIHM.java
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/DashboardFenetre/GlobalIHM.java
@@ -30,21 +30,16 @@ public class GlobalIHM extends JFrame {
     private final static Logger logger = LoggerFactory.getLogger(LoggingLabel);
     private final static String networkConfigFile = "network.yaml";
 
-    // Composants pour la sélection de date
     private JDatePickerImpl datePickerStart;
     private JDatePickerImpl datePickerEnd;
 
-    // Récupérer les dates sélectionnées dans les JDatePickerImpl
     Date startDate = (Date) datePickerStart.getModel().getValue();
     Date endDate = (Date) datePickerEnd.getModel().getValue();
 
-    // Composant pour la sélection du code postal
     private JComboBox<String> codePostalComboBox;
     
-    // Composants graphiques
     private JPanel chartPanel;
 
-    // Variable pour stocker le code postal
     private String codePostal;
 
     public GlobalIHM() throws InterruptedException, IOException {
@@ -55,13 +50,11 @@ public class GlobalIHM extends JFrame {
         
         initializeComponents();
         setVisible(true);
-        // Charger la configuration du réseau
+
         NetworkConfig networkConfig = ConfigLoader.loadConfig(NetworkConfig.class, networkConfigFile);
 
-        // Créer l'objet de filtre avec les paramètres
         DashboardFilterDTO filterDTO = new DashboardFilterDTO(startDate, endDate, codePostal);
         
-        // Appeler le service global pour obtenir les données filtrées
         DashboardServiceGlobal dashboardService = new DashboardServiceGlobal(networkConfig);
         GlobalDatas globalDatas = dashboardService.global(filterDTO);
         try {
@@ -69,12 +62,12 @@ public class GlobalIHM extends JFrame {
         } catch (InterruptedException e) {
             e.printStackTrace();
         } 
-        // Mettre à jour les graphiques
+
         updateCharts(globalDatas);
     }
 
     private void initializeComponents() {
-        // Initialisation des modèles de date
+
         UtilDateModel modelStart = new UtilDateModel();
         UtilDateModel modelEnd = new UtilDateModel();
         
@@ -84,7 +77,6 @@ public class GlobalIHM extends JFrame {
         datePickerStart = new JDatePickerImpl(datePanelStart, null);
         datePickerEnd = new JDatePickerImpl(datePanelEnd, null);
 
-        // Panel de sélection des dates
         JPanel datePanel = new JPanel();
         datePanel.setLayout(new FlowLayout());
 
@@ -94,47 +86,39 @@ public class GlobalIHM extends JFrame {
         datePanel.add(new JLabel("Date de fin:"));
         datePanel.add(datePickerEnd);
 
-        // ComboBox pour la sélection du code postal
         JLabel codePostalLabel = new JLabel("Code Postal:");
-        codePostalComboBox = new JComboBox<>(new String[] {"75000", "75001", "75002"});  // Ajoutez les codes postaux nécessaires
+        codePostalComboBox = new JComboBox<>(new String[] {"75000", "75001", "75002"});
         datePanel.add(codePostalLabel);
         datePanel.add(codePostalComboBox);
 
-        // Bouton appliquer les filtres
         JButton applyButton = new JButton("Appliquer");
         applyButton.addActionListener(e -> {
             try {
                 applyFilters();
             } catch (IOException e1) {
-                // TODO Auto-generated catch block
                 e1.printStackTrace();
             } catch (InterruptedException e1) {
-                            // TODO Auto-generated catch block
                             e1.printStackTrace();
                         }
         });
         datePanel.add(applyButton);
 
-        // Ajouter le panel des dates à la fenêtre
         add(datePanel, BorderLayout.NORTH);
 
-        // Section de statistiques
         JPanel statsPanel = new JPanel();
         statsPanel.setLayout(new GridLayout(2, 1));
 
-        // Graphiques (camemberts et histogrammes)
         chartPanel = new JPanel();
         chartPanel.setLayout(new GridLayout(2, 2));
         statsPanel.add(chartPanel);
 
-        // Ajouter le panel des statistiques à la fenêtre
         add(statsPanel, BorderLayout.CENTER);
     }
 
     private void applyFilters() throws IOException, InterruptedException {
         Date startDate = (Date) datePickerStart.getModel().getValue();
         Date endDate = (Date) datePickerEnd.getModel().getValue();
-        codePostal = (String) codePostalComboBox.getSelectedItem();  // Récupérer le code postal sélectionné
+        codePostal = (String) codePostalComboBox.getSelectedItem();
 
         if (startDate == null || endDate == null) {
             JOptionPane.showMessageDialog(this, "Veuillez sélectionner des dates valides.");
@@ -146,10 +130,7 @@ public class GlobalIHM extends JFrame {
 
     @SuppressWarnings("unchecked")
     private void updateCharts(GlobalDatas globalDatas) {
-        chartPanel.removeAll();  // Vider le panel avant d'ajouter de nouveaux graphiques
-        //statsPanel.removeAll();
-
-        // Initialisation des variables pour l'agrégation des données
+        chartPanel.removeAll(); 
 
         int totalIncidents = 0; int totalSuggestions = 0;
 
@@ -161,20 +142,19 @@ public class GlobalIHM extends JFrame {
 
         int incidentLevelBas = 0; int incidentLevelMoyen = 0; int incidentLevelHaut = 0; int incidentNonDefini = 0;
         
-        // Itération sur tous les objets GlobalData dans GlobalDatas
+   
         for (GlobalData globalData : globalDatas.getGlobalDataSet()) {
-            // Agregation pour totaux
+      
             totalIncidents += globalData.getTotalIncidents();
             totalSuggestions += globalData.getTotalSuggestion();
-            // Agrégation des incidents
+          
             incidentVoirie += globalData.getIncidentCatVoirie();
             incidentEclairagePublic += globalData.getIncidentCatEclairagePublic();
             incidentEspaceVerts += globalData.getIncidentCatEspaceVerts();
             incidentProprete += globalData.getIncidentCatProprete();
             incidentAnimauxErrants += globalData.getIncidentCatAnimauxErrants();
             incidentAutres += globalData.getIncidentCatAutres();
-            
-            // Agrégation des suggestions
+        
             suggestionVoirie += globalData.getSuggestionCatVoirie();
             suggestionEclairagePublic += globalData.getSuggestionCatEclairagePublic();
             suggestionEspaceVerts += globalData.getSuggestionCatEspaceVerts();
@@ -182,14 +162,12 @@ public class GlobalIHM extends JFrame {
             suggestionAnimauxErrants += globalData.getSuggestionCatAnimauxErrants();
             suggestionAutres += globalData.getSuggestionCatAutres();
 
-            // Agregation pour incident par priorité
             incidentLevelBas += globalData.getIncidentLevelBas();
             incidentLevelMoyen += globalData.getIncidentLevelMoyen();
             incidentLevelHaut += globalData.getIncidentLevelHaut();
             incidentNonDefini += globalData.getIncidentNonDefini();
         }
 
-        // Mise à jour du tableau pour les totaux
         String[] columnNames = {"Statistique", "Valeur"};
         Object[][] data = {
             {"Nombre total d'incidents", totalIncidents},
@@ -204,7 +182,6 @@ public class GlobalIHM extends JFrame {
         statsTable.add(scrollPane);
         chartPanel.add(statsTable);
     
-        // Mise à jour du graphique camembert pour les incidents
         @SuppressWarnings("rawtypes")
         DefaultPieDataset incidentDataset = new DefaultPieDataset();
         incidentDataset.setValue("Voirie", incidentVoirie);
@@ -217,7 +194,6 @@ public class GlobalIHM extends JFrame {
         ChartPanel incidentChartPanel = new ChartPanel(incidentChart);
         chartPanel.add(incidentChartPanel);
     
-        // Mise à jour du graphique camembert pour les suggestions
         @SuppressWarnings("rawtypes")
         DefaultPieDataset suggestionDataset = new DefaultPieDataset();
         suggestionDataset.setValue("Voirie", suggestionVoirie);
@@ -229,8 +205,7 @@ public class GlobalIHM extends JFrame {
         JFreeChart suggestionChart = ChartFactory.createPieChart("Répartition des Suggestions par Catégorie", suggestionDataset, true, true, false);
         ChartPanel suggestionChartPanel = new ChartPanel(suggestionChart);
         chartPanel.add(suggestionChartPanel);
-    
-        // Mise à jour de l'histogramme pour les incidents par niveau de priorité
+
         DefaultCategoryDataset priorityDataset = new DefaultCategoryDataset();
         priorityDataset.addValue(incidentLevelBas, "Incident", "Basse");
         priorityDataset.addValue(incidentLevelMoyen, "Incident", "Moyenne");
@@ -240,7 +215,6 @@ public class GlobalIHM extends JFrame {
         ChartPanel priorityChartPanel = new ChartPanel(priorityChart);
         chartPanel.add(priorityChartPanel);
     
-        // Repeindre le panel des graphiques
         chartPanel.revalidate();
         chartPanel.repaint();
     }
@@ -251,10 +225,8 @@ public class GlobalIHM extends JFrame {
             try {
                 new GlobalIHM();
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         });
