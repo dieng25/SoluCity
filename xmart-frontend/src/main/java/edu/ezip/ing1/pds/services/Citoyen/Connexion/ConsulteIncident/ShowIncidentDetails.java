be0f7cf63d4a5ae9f13c4279e67f7cf1b54f0000
GlabGlab <claraadamaly@gmail.com>
2025-03-20 16:23:45 +0100

    conversion des priorités en texte

--- xmart-frontend/src/main/java/edu/ezip/ing1/pds/services/Citoyen/Connexion/ConsulteIncident/ShowIncidentDetails.java
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/services/Citoyen/Connexion/ConsulteIncident/ShowIncidentDetails.java
@@ -15,12 +15,27 @@ public class ShowIncidentDetails extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new GridLayout(0, 1));
 
-        add(new JLabel("Priorité: " + incident.getPriorite()));
+        add(new JLabel("Priorité: " + getPrioriteDescription(incident.getPriorite())));
         add(new JLabel("Description: " + incident.getDescription()));
 
         setVisible(true);
 
 
 
+    }
+
+    private String getPrioriteDescription(int priorite) {
+        switch (priorite) {
+            case 0:
+                return "Pas de priorité définie";
+            case 1:
+                return "Faible";
+            case 2:
+                return "Moyen";
+            case 3:
+                return "Élevée";
+            default:
+                return "Inconnue";
+        }
     }
 }
